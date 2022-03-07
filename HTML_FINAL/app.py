5752bb77bba6e43f632271ff2a02eb32aa838980
mgiandello <michaela.giandello@gmail.com>
2022-03-07 15:51:24 -0500

    edits

--- HTML_FINAL/app.py
+++ HTML_FINAL/app.py
@@ -20,13 +20,13 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
 # # Remove tracking modifications
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 # Pet = create_classes(db)
 
